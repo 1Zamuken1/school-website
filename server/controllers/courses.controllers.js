e20c562db612a4c33816be0a36fa0c11181f4bbe
Juan Barrios <juanbarrios072@gmail.com>
2023-08-22 12:31:18 -0500

    error handling

--- server/controllers/courses.controllers.js
+++ server/controllers/courses.controllers.js
@@ -2,10 +2,14 @@ import { pool } from "../db.js";
 
 // Obtener todos los cursos
 export const getCourses = async (req, res) => {
-  const [result] = await pool.query(
-    "SELECT * FROM courses ORDER BY course_title ASC"
-  );
-  res.json(result);
+  try {
+    const [result] = await pool.query(
+      "SELECT * FROM courses ORDER BY course_title ASC"
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 // Obtener un curso en específico
@@ -13,27 +17,29 @@ export const getCourse = async (req, res) => {
   const [result] = await pool.query("SELECT * FROM courses WHERE code = ?", [
     req.params.code,
   ]);
-
   // Devolver error si no existe el curso solicitado
   if (result.length === 0) {
-    return res.status(404).json({ message: "Task not found" });
+    return res.status(404).json({ message: "Course not found" });
   }
-
   res.json(result[0]);
 };
 
 // Crear curso
 export const createCourse = async (req, res) => {
-  const { course_title, course_description } = req.body;
-  const [result] = await pool.query(
-    "INSERT INTO courses (course_title, course_description) VALUES (?, ?)",
-    [course_title, course_description]
-  );
-  res.json({
-    id: result.insertId,
-    course_title,
-    course_description,
-  });
+  try {
+    const { course_title, course_description } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO courses (course_title, course_description) VALUES (?, ?)",
+      [course_title, course_description]
+    );
+    res.json({
+      id: result.insertId,
+      course_title,
+      course_description,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 // Actualizar curso
